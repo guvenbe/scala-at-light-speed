5afc8b1ed9d6db1dd1a2a01a6fc5cb91ebde2c3e
Bora Guven <guvenbe@gmail.com>
2020-01-21 00:24:11 -0500

    completed OO

---
+++ src/main/scala/lectures/part2oop/Generics.scala
@@ -0,0 +1,45 @@
+package lectures.part2oop
+
+object Generics extends App {
+
+  class MyList[A]{
+  //use the type A
+  }
+
+  class MyMAP[Key, Value]
+  val listOfIntegers = new MyList[Int]
+  val listOfStrings = new MyList[String]
+
+  //generic methods
+  object MyList {
+    def empty[A] :MyList[A] = ???
+  }
+
+  val empyListOfIntegers = MyList.empty[Int]
+
+  //varienace problem
+
+  class Animal
+  class Cat extends Animal
+  class Dog extends Animal
+
+  //1. yes List[Cat] extends List[Animal] = COVARIANCE
+  class CovariantList[+A]
+  val animal: Animal = new Cat
+  val animalList: CovariantList[Animal] = new CovariantList[Cat]
+  //animalList.add(new Dog) ??? HARD QUESTION
+
+  //2. no= INVARIANCE
+  class InvariantList[A]
+  val invariantList: InvariantList[Animal] = new InvariantList[Animal]
+
+  //3. Hell no!
+  class Trainer[-A]
+  val trainer: Trainer[Cat] = new Trainer[Animal]
+
+  //bounded types
+  class Cage[A<: Animal](animal: A)
+  val cage = new Cage(new Dog)
+
+
+}
